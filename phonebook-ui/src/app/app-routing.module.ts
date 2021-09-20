5b2fd36a5edb17288a154d3ac5ccc78fcc631191
andREutow <andre.reutow@gmail.com>
2021-09-20 19:46:48 +0200

    resolve conflicts

--- phonebook-ui/src/app/app-routing.module.ts
+++ phonebook-ui/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ContactsComponent} from "./pages/contacts/contact-list/contacts.component";
-import {ContactDetailsComponent} from "./pages/contact-details/contact-details.component";
+import {ContactDetailsPageComponent} from "./pages/contact-details-page/contact-details-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/contacts', pathMatch: 'full'},
   {path: 'contacts', component: ContactsComponent},
-  {path: 'details/:id', component: ContactDetailsComponent},
+  {path: 'contacts/:id', component: ContactDetailsPageComponent},
 
-  {path: 'contacts/favorites', component: ContactDetailsComponent},
-  {path: 'contacts/groups', component: ContactDetailsComponent},
-  {path: 'contacts/profile', component: ContactDetailsComponent},
-  {path: 'logout', component: ContactDetailsComponent},
+  {path: 'contacts/favorites', component: ContactDetailsPageComponent},
+  {path: 'contacts/groups', component: ContactDetailsPageComponent},
+  {path: 'contacts/profile', component: ContactDetailsPageComponent},
+  {path: 'logout', component: ContactDetailsPageComponent},
 ];
 
 @NgModule({
