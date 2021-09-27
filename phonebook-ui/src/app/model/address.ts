4886c0a8fe9912e2c4dbac67a16f992ef57e517c
marinamitunevich <83472675+marinamitunevich@users.noreply.github.com>
2021-09-27 13:18:44 +0200

    PB-25-4 add edit address (#30)

--- phonebook-ui/src/app/model/address.ts
+++ phonebook-ui/src/app/model/address.ts
@@ -2,9 +2,8 @@ export interface Address {
   id : number,
   country: string,
   city: string,
+  address: string,
   index: string,
-  street: string,
-  homeNr: number,
   isFavorite: boolean,
   contactId: number
 }
--- phonebook-ui/src/app/pages/contact-details-page/contact-addresses/address-details/address-details.component.ts
+++ phonebook-ui/src/app/pages/contact-details-page/contact-addresses/address-details/address-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input,} from '@angular/core';
 import {Address} from "../../../../model/address";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {AddAndEditAddressFormComponent} from "../../../modalwindows/add-and-edit-address-form/add-and-edit-address-form.component";
 
 @Component({
   selector: 'app-address-details',
@@ -11,4 +13,15 @@ export class AddressDetailsComponent {
   @Input()
   address: Address | undefined;
 
+  constructor(private modalService: NgbModal) {
+  }
+
+  open() {
+    const modalRef = this.modalService.open(AddAndEditAddressFormComponent);
+
+    modalRef.componentInstance.address = this.address;
+    modalRef.componentInstance.artOfForm = "Edit your address"
+    modalRef.closed.subscribe(value => this.address = value);
+
+  }
 }
